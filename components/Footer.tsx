594ef58f675af17e4b0eb2bdaf19ebcd19ed6b76
tj <tharunkumar200230@gmail.com>
2023-07-01 06:59:20 +0530

    added searchbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+import NextLink from "next/link";
 import { footerLinks } from "@/constants";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -20,12 +21,31 @@ const Footer = () => {
           </p>
         </div>
         <div className="footer__links">
-          {footerLinks.map((Link)=>(
+          {footerLinks.map((Link) => (
             <div key={Link.title} className="footer__link">
-                <h3 className="font-bold">{Link.title}</h3>
-                {}
+              <h3 className="font-bold">{Link.title}</h3>
+              {Link.links.map((item) => (
+                <NextLink
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </NextLink>
+              ))}
             </div>
           ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub.All rights reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms Of Use
+          </Link>
         </div>
       </div>
     </footer>
